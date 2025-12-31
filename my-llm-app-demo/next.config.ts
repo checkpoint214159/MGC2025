4a2fd20e011d7737fc851391dd7030f12c252ed7
checkpoint214159 <bengoh04@gmail.com>
2025-12-31 14:24:07 +0800

    removed webpack

--- my-llm-app-demo/next.config.ts
+++ my-llm-app-demo/next.config.ts
@@ -1,16 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    webpack: (config, { isServer, dev }) => {
-    if (dev && !isServer) {
-      config.watchOptions = {
-        poll: 300, // Check every 300ms (adjust as needed)
-        aggregateTimeout: 300,
-        ignored: /node_modules/,
-      };
-    }
-    return config;
-  },
+    typescript: {
+        ignoreBuildErrors: false, 
+    },
 };
 
 export default nextConfig;
