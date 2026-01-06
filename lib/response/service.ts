9b5ba13db195330fd646c7262f7dac1a78811426
checkpoint214159 <bengoh04@gmail.com>
2026-01-06 13:09:40 +0800

    partially adding new db fields for actual LLM integration

---
+++ lib/response/service.ts
@@ -0,0 +1,10 @@
+
+
+
+export function QuestionFieldsGenerator() {
+    /**
+     * Generates questioning fields that will continually prompt user for new input if more specifics
+     * are needed. 
+     */
+}
+    use
